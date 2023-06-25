172a3041064c0af810c23f17fbdff089e3c2b4f4
Rain <rainymorning0000@gmail.com>
2023-06-25 14:57:38 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,25 +1,4 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
-
-/**
- * print_number - Prints an integer recursively.
- * @n: The integer to print.
- *
- * Return: The number of digits printed.
- */
-int print_number(int n)
-{
-    int count = 0;
-
-    if (n / 10)
-        count += print_number(n / 10);
-
-    putchar((n % 10) + '0');
-    count++;
-
-    return count;
-}
 
 /**
  * _printf - Produces output according to a format.
@@ -29,83 +8,83 @@ int print_number(int n)
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+	va_list args;
+	int count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					{
+						int c = va_arg(args, int);
 
-    if (format == NULL)
-        return (-1);
+						putchar(c);
+						count++;
+						break;
+					}
+				case 's':
+					{
+						char *str = va_arg(args, char *);
+						int i;
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++; /* Move past the '%' */
+						if (str == NULL)
+							str = "(null)";
+						for (i = 0; str[i] != '\0'; i++)
+						{
+							putchar(str[i]);
+							count++;
+						}
+						break;
+					}
+				case 'd':
+				case 'i':
+					{
+						int num = va_arg(args, int);
+						int divisor = 1;
 
-            /* Handle the conversion specifier */
-            switch (*format)
-            {
-                case 'c':
-                {
-                    int c = va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case 's':
-                {
-                    char *str = va_arg(args, char *);
-                    int i;
-
-                    if (str == NULL)
-                        str = "(null)";
-
-                    for (i = 0; str[i] != '\0'; i++)
-                    {
-                        putchar(str[i]);
-                        count++;
-                    }
-                    break;
-                }
-                case 'd':
-                case 'i':
-                {
-                    int num = va_arg(args, int);
-
-                    if (num < 0)
-                    {
-                        putchar('-');
-                        count++;
-                        num = -num;
-                    }
-
-                    count += print_number(num);
-                    break;
-                }
-                case '%':
-                {
-                    putchar('%');
-                    count++;
-                    break;
-                }
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-
-        format++; /* Move to the next character in the format string */
-    }
-
-    va_end(args);
-
-    return count;
+						if (num < 0)
+						{
+							putchar('-');
+							count++;
+							num = -num;
+						}
+						while (num / divisor > 9)
+							divisor *= 10;
+						while (divisor != 0)
+						{
+							putchar((num / divisor) % 10 + '0');
+							count++;
+							divisor /= 10;
+						}
+						break;
+					}
+				case '%':
+					{
+						putchar('%');
+						count++;
+						break;
+					}
+				default:
+					putchar('%');
+					putchar(*format);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
